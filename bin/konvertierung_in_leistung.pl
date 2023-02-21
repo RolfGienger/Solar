4f183084c30cdb6099aa1a39a3b4122b20c99bce
RolfGienger <rolf.gienger@web.de>
2023-02-21 18:00:04 +0100

    zusaetzliche Spalte mit min. Leistung hinzu

--- bin/konvertierung_in_leistung.pl
+++ bin/konvertierung_in_leistung.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# @(#) $Revision: 1.00 $
+# @(#) $Revision: 1.10 $
 #
 # Zweck     : Konvertierung einer Eisntrahlungsdatei in einen Leistungsverlauf
 #
@@ -15,6 +15,7 @@
 #
 # Version    Datum        Name    Aenderung
 # 1.0        18.02.2023   Gienger Erstellung
+# 1.1        21.02.2023   Gienger dritte Spalte mit Nullwerten hinzu
 #
 #
 
@@ -35,52 +36,52 @@ my $pout = 0;
 GetOptions("pmax=i" => \$pmax) or die("wrong options!\n");
 
 
-print("Stunde;Leistung in kW\n");
+print("Stunde;max. Leistung in kW; min. Leistung in kW\n");
 
 while (<>) {
 	chomp();
 	($timefromfile, $value) = split(/;/);
 	if ($value < 30) {
-		print("$day;0.0\n");
+		print("$day;0.0;0.0\n");
 	}
 	if (($value >= 30) && ($value < 36)) {
 		$pout = $pmax * 0.1;
-		print("$day;$pout\n");
+		print("$day;$pout;0.0\n");
 	}
 	if (($value >= 36) && ($value < 42)) {
 		$pout = $pmax * 0.2;
-		print("$day;$pout\n");
+		print("$day;$pout;0.0\n");
 	}
 	if (($value >= 42) && ($value < 48)) {
 		$pout = $pmax * 0.3;
-		print("$day;$pout\n");
+		print("$day;$pout;0.0\n");
 	}
 	if (($value >= 48) && ($value < 54)) {
 		$pout = $pmax * 0.4;
-		print("$day;$pout\n");
+		print("$day;$pout;0.0\n");
 	}
 	if (($value >= 54) && ($value < 60)) {
 		$pout = $pmax * 0.5;
-		print("$day;$pout\n");
+		print("$day;$pout;0.0\n");
 	}
 	if (($value >= 60) && ($value < 66)) {
 		$pout = $pmax * 0.6;
-		print("$day;$pout\n");
+		print("$day;$pout;0.0\n");
 	}
 	if (($value >= 66) && ($value < 72)) {
 		$pout = $pmax * 0.7;
-		print("$day;$pout\n");
+		print("$day;$pout;0.0\n");
 	}
 	if (($value >= 72) && ($value < 78)) {
 		$pout = $pmax * 0.8;
-		print("$day;$pout\n");
+		print("$day;$pout;0.0\n");
 	}
 	if (($value >= 78) && ($value < 84)) {
 		$pout = $pmax * 0.9;
-		print("$day;$pout\n");
+		print("$day;$pout;0.0\n");
 	}
 	if ($value >= 84) {
-		print("$day;$pmax\n");
+		print("$day;$pmax;0.0\n");
 	}
 	$day++;
 }
